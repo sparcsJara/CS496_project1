45abc31d8195de0432feb21c944adcbd6c41264c
HwangheeKim <hwangheek@gmail.com>
2016-12-27 19:23:38 +0900

    Implent tab functionality

---
+++ app/src/main/java/com/example/q/cs496_android/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.q.cs496_android;
+
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+implements ContactsFragment.OnFragmentInteractionListener,
+        GalleryFragment.OnFragmentInteractionListener, OurFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabAdapter tabAdapter = new TabAdapter(getSupportFragmentManager());
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        tabAdapter.addFragment(new ContactsFragment(), "Contacts");
+        tabAdapter.addFragment(new GalleryFragment(), "Gallery");
+        tabAdapter.addFragment(new OurFragment(), "CHANGE THIS");
+        viewPager.setAdapter(tabAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ app/src/main/java/com/example/q/cs496_android/TabAdapter.java
@@ -0,0 +1,41 @@
+package com.example.q.cs496_android;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by q on 2016-12-27.
+ */
+
+public class TabAdapter extends FragmentPagerAdapter {
+
+    private final ArrayList<Fragment> fragments = new ArrayList<>();
+    private final ArrayList<String> fragmentTitles = new ArrayList<>();
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragments.add(fragment);
+        fragmentTitles.add(title);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitles.get(position);
+    }
+}
